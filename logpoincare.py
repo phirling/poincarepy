da30b05dc16d18afaedc68e59bf692d8f1e7085e
Patrick Hirling <90903147+hyperfeinstruktur@users.noreply.github.com>
2022-09-26 18:07:00 +0200

    Add files via upload

---
+++ logpoincare.py
@@ -0,0 +1,267 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.integrate as scpint
+import argparse
+from tqdm import tqdm
+import solver
+
+fs = (15,7)
+
+parser = argparse.ArgumentParser(
+    description="Poincare x vx Section of Logarithmic potential"
+)
+'''
+The program has 2 modes: by default, no orbits are drawn and the program is in an interactive mode where
+clicking on a point on the left plane (x,vx) sets the IC, draws an orbit and the corresponding Poincaré section.
+If the --fill flag is passed, the program calculates a given number of orbits whose starting points lie uniformly
+on the x axis (with vx=0) and then becomes interactive, where clicking on a poincaré curve shows its corresponding
+orbit in xy-space on the right.
+
+For the integration duration, the user can either (default) specify a number of crossings of the y plane (i.e. a number
+of points in the Poincaré map), in which case the -tf argument is used as an upper limit or timeout to the integration time,
+or the user can specify --no_count in which case the integration will keep going until tf is reached.
+'''
+
+parser.add_argument("-v0",type=float,default=10.,help="Characteristic Velocity")
+parser.add_argument("-rc",type=float,default=1.,help="Characteristic Radius")
+parser.add_argument("-q",type=float,default=0.8,help="Flattening Parameter")
+parser.add_argument("-E",type=float,default=80,help="Energy of Orbit")
+
+parser.add_argument("--jac",action='store_true',help="Compute the Jacobian")
+
+parser.add_argument("-tf",type=float,default=100,help="Maximal integration time. If --no_count, this will be the obligatory final time")
+parser.add_argument("--no_count",action='store_true',help="Integrate until t=tf, without counting the crossings")
+parser.add_argument("-nb_crossings",type=int,default= 40,help="Terminate integration after n crossings of the plane (=nb of points in the Poincaré map)")
+parser.add_argument("--fill",action='store_true',help="Try to fill the phase space with orbits")
+parser.add_argument("-nb_orbs",type=int,default=11,help="Number of orbits to sample if --fill is passed")
+
+args = parser.parse_args()
+
+# Define Physical System
+G = 4.299581e04
+class LogarithmicPotential:
+    def __init__(self,v0,rc,q):
+        self.v0 = v0
+        self.rc = rc
+        self.q = q
+
+    def potential(self,y):
+        return 0.5*self.v0**2 * np.log(self.rc**2 + y[0]**2 + y[1]**2/self.q**2)
+
+    def accel(self,y):
+        A = - self.v0**2 / (self.rc**2 + y[0]**2 + y[1]**2/self.q**2)
+        ax = A*y[0]
+        ay = A*y[1]/(self.q**2)
+        return np.array([ax,ay])
+
+    # Bounds on the zero velocity curve
+    # xdot = ydot = y = 0
+    def maxval_x(self,E):
+        return np.sqrt(np.exp(2*E/self.v0**2)-self.rc**2)
+    # xdot = ydot = x = 0
+    def maxval_y(self,E):
+        return np.sqrt(np.exp(2*E/self.v0**2)-self.rc**2) * self.q
+
+pot = LogarithmicPotential(args.v0,args.rc,args.q)
+
+# RHS of the system dy/dt = f(y) where y = (x,y,vx,vy)
+def RHS(t,y):
+    F = pot.accel(y)
+    return [y[2],
+            y[3],
+            F[0],
+            F[1]]
+
+# Compute zero-velocity curve at E fixed
+xlim = 0.9999*pot.maxval_x(args.E)
+def xdot_of_x(x):
+    Ed = args.E - pot.potential([x,0])
+    if np.any(Ed < 0):
+        raise ValueError("Outside of bounds")
+    return np.sqrt(2*Ed)
+xsp = np.linspace(-xlim,xlim,200)
+xdotsp = xdot_of_x(xsp)
+
+# Initialize Figure
+# Left panel (ax[0]): x vx space with y=0 v_y > 0 : the Poincaré map space
+# Right panel: configuration space (xy)
+fig, ax = plt.subplots(1,2,figsize=fs)
+ax[0].set_xlabel('$x$ [kpc]')
+ax[0].set_ylabel('$v_x$ [km/s]')
+ax[1].set_xlabel('$x$ [kpc]')
+ax[1].set_ylabel('$y$ [kpc]')
+ax[0].set_title("Poincaré section $y=0,v_y>0$")
+ax[1].set_title("Orbit in $xy$-plane")
+fig.suptitle('Logarithmic Potential, $q=$ {:.2f}, $E=$ {:.1f}'.format(args.q,args.E),fontsize=16)
+
+# Zero velocity curve in Poincaré space
+ax[0].plot(xsp,xdotsp)
+ax[0].plot(xsp,-xdotsp)
+
+# Text box to hold information
+txt = ax[0].text(0.2, 0.9,'',
+     horizontalalignment='center',
+     verticalalignment='center',
+     transform = ax[0].transAxes)
+
+# Fix limit of the right panel
+llim = 1.1*xlim
+ax[1].set_xlim(-llim,llim)
+ax[1].set_ylim(-llim,llim)
+
+# Integration Parameters
+tf = args.tf
+t_span = (0,tf)
+nb_points_orbit = 20000
+t_eval = np.linspace(0,tf,nb_points_orbit) # <-- TODO: more efficient solution here
+if args.no_count:
+    event_count_max = None
+else:
+    event_count_max = args.nb_crossings
+
+# Function that checks if y=0 with fixed direction of crossing (Vy>0)
+def event_yplanecross(t,y):
+    return y[1]
+event_yplanecross.direction = 1
+
+# Main program: either single-map interactive mode or auto-filled multi-map mode
+# Mode 1:
+if not args.fill:
+
+    txt.set_text('Click on a point\nto start')
+
+    # Create empty lines in both panels
+    line_sections, = ax[0].plot([], [],'o',ms=1.5)
+    line_orbits, = ax[1].plot([], [],lw=1)
+
+    # Picking function used to get initial x, vx from user input
+    def onpick(event):
+        if event.inaxes == ax[0]:
+            # x, xdot IC
+            x, xdot = event.xdata, event.ydata
+            # Orbit starts on y=0 plane
+            y = 0
+
+            # Calculate ydot from fixed E and x, xdot
+            ED = 2*(args.E-pot.potential([x,y])) - xdot**2
+            if ED >= 0:
+                ydot = np.sqrt(ED)
+                y0 = [x,y,xdot,ydot] 
+                #res = scpint.solve_ivp(RHS,tsp,y0,t_eval=t_eval,method='DOP853',events=event_yplanecross)
+                res = solver.integrate_orbit(RHS,t_span,y0,t_eval=t_eval,events=event_yplanecross,event_count_end=event_count_max)
+
+                # Orbit Output
+                xx = res['y'][0]
+                yy = res['y'][1]
+
+                # Ouput of event watcher
+                yevs = res['y_events'][0]
+                X = yevs[:,0]
+                Xdot = yevs[:,2]
+
+                # TODO: Compute jacobian etc
+                if args.jac:
+                    ddiv = 1000000.
+                    dx = abs(x/ddiv)
+                    dxdot = abs(xdot/ddiv)
+                    y01 = [x+dx,y,xdot,ydot]
+                    y02 = [x,y,xdot+dxdot,ydot]
+                    res01 = scpint.solve_ivp(RHS,t_span,y01,method='DOP853',events=event_yplanecross)
+                    res02 = scpint.solve_ivp(RHS,t_span,y02,method='DOP853',events=event_yplanecross)
+
+                    yevs01 = res01['y_events'][0]
+                    yevs02 = res02['y_events'][0]
+
+                    ii = 1
+                    J00 = (yevs01[:,0][ii] - yevs[:,0][ii])/ dx
+                    J01 = (yevs01[:,2][ii] - yevs[:,2][ii])/ dx
+                    J10 = (yevs02[:,0][ii] - yevs[:,0][ii])/ dxdot
+                    J11 = (yevs02[:,2][ii] - yevs[:,2][ii])/ dxdot
+
+                    Jac = np.array([[J00,J01],[J10,J11]])
+                    print(Jac)
+
+                # Plot Poincaré section & Orbit in xy space
+                line_sections.set_xdata(X)
+                line_sections.set_ydata(Xdot)
+
+                line_orbits.set_xdata(xx)
+                line_orbits.set_ydata(yy)
+
+                # Show info
+                txt.set_text('IC:\n$x=$ {:.2f} kpc\n$v_x=$ {:.2f} km/s\n$v_y=$ {:.2f} km/s'.format(x,xdot,ydot))
+                fig.canvas.draw()
+            else:
+                print("Point outside of zero-velocity curve")
+
+    fig.canvas.mpl_connect('button_press_event', onpick)
+
+# Mode 2: Draw a set of orbits in advance, show xy-orbit by clicking
+else:
+    # Create empty line in the right panel (the left panel will be filled)
+    line_orbits, = ax[1].plot([], [],lw=1)
+    
+    # Interactivity is handled by a class (cleaner & allows some visual touches like colorizing the picked curve)
+    class OrbitPicker:
+        def __init__(self,line_orbits):
+            self.line_orbits = line_orbits
+            self.sections = [] # <- list of line2d objects: the poincaré sections in the left panel
+            self.orbits_x = [] # <- list of arrays containing the x/y coords of the orbits
+            self.orbits_y = [] #    These are plotted when the corresponding section in the left panel is clicked
+            self.firstpick = True
+            line_orbits.figure.canvas.mpl_connect('pick_event',self)
+
+        def append_orbit(self,x,y):
+            self.orbits_x.append(x)
+            self.orbits_y.append(y)
+        
+        def append_section(self,secline):
+            self.sections.append(secline)
+        
+        def __call__(self,event):
+            if self.firstpick:
+                txt.set_text('')
+                self.firstpick = False
+            else:
+                self.prev_artist.set_color('black')
+                self.prev_artist.set_markersize(1)
+
+            event.artist.set_color('turquoise')
+            event.artist.set_markersize(2)
+            k = event.artist.id # <- When the section is plotted (see below), an "ID" attribute is set to
+                                #    match it with its corresponding orbit. TODO: find a clearer approach?
+            self.line_orbits.set_xdata(self.orbits_x[k])
+            self.line_orbits.set_ydata(self.orbits_y[k])
+            self.line_orbits.figure.canvas.draw()
+            self.prev_artist = event.artist
+
+    # Sample ICs uniformly on x axis
+    x_ic = np.linspace(-xlim,xlim,args.nb_orbs)
+    ydot_ic = np.empty(args.nb_orbs)
+    for i,x in enumerate(x_ic):
+        ydot_ic[i] = np.sqrt(2*(args.E-pot.potential([x_ic[i],0])))
+    
+    
+    txt.set_text('Click on a curve to\nshow its orbit')
+    Picker = OrbitPicker(line_orbits)
+
+    # Integrate the nb_orbs trajectories
+    for k in tqdm(range(args.nb_orbs)):
+        y0 = [x_ic[k],0,0,ydot_ic[k]]
+        #res = scpint.solve_ivp(RHS,tsp,y0,t_eval=t_eval,method='DOP853',events=event_yplanecross)
+        res = solver.integrate_orbit(RHS,t_span,y0,t_eval=t_eval,events=event_yplanecross,event_count_end=event_count_max)
+
+        # Output the orbits to be displayed upon clicking
+        Picker.append_orbit(res['y'][0],res['y'][1])
+
+        # Ouput of event watcher
+        yevs = res['y_events'][0]
+        X = yevs[:,0]
+        Xdot = yevs[:,2]
+
+        # Plot Poincaré curve and set its ID to pair it with corresponding orbit
+        secline, = ax[0].plot(X, Xdot,'o',ms=0.7,color='black',picker=True)
+        secline.id = k
+        Picker.append_section(secline)
+
+plt.show()
---
+++ solver.py
@@ -0,0 +1,120 @@
+import numpy as np
+from scipy.integrate._ivp.rk import DOP853
+from scipy.integrate._ivp.ivp import prepare_events, handle_events, find_active_events
+from scipy.optimize import OptimizeResult
+
+'''
+Custom wrapper around scipy's DOP853 integrator, largely inspired by the solve_ivp method
+but containing some additional features and discarding functionalities not required by this program.
+
+DISCLAIMER: Some portions of the code are taken litteraly from scipy's source,
+            specifically scipy/integrate/ivp.py).
+
+Newly added:
+The user can either specify a time span (ti,tf) for the integration OR specify a number of times a
+given event (typically a plane crossing) must happen for the integration to stop.
+
+Limitations (so far):
+Only deal with one event type.
+
+Parameters
+---------
+fun: callable
+
+'''
+
+# The following function is a remade version of solve_ivp, which reuses some parts of the original
+def integrate_orbit(fun, t_span, y0, t_eval=None, events=None,event_count_end = None, **options):
+    t0, tf = map(float,t_span)
+    solver = DOP853(fun,t0,y0,tf,**options)
+
+    if t_eval is None:
+        ts = [t0]
+        ys = [y0]
+    else:
+        ts = []
+        ys = []
+
+    t_eval_i = 0
+
+    events, is_terminal, event_dir = prepare_events(events)
+
+    if events is not None:
+        g = [event(t0, y0) for event in events]
+        t_events = [[] for _ in range(len(events))]
+        y_events = [[] for _ in range(len(events))]
+    else:
+        t_events = None
+        y_events = None
+
+    status = None
+    event_count = 0
+
+    while status is None:
+        message = solver.step()
+        if solver.status == 'finished':
+            status = 0
+        elif solver.status == 'failed':
+            status = -1
+            break
+
+        t_old = solver.t_old
+        t = solver.t
+        y = solver.y
+        sol = None
+
+        if events is not None:
+            g_new = [event(t, y) for event in events]
+            active_events = find_active_events(g, g_new, event_dir)
+            if active_events.size > 0:
+                if sol is None:
+                    sol = solver.dense_output()
+
+                root_indices, roots, terminate = handle_events(
+                    sol, events, active_events, is_terminal, t_old, t)
+                for e, te in zip(root_indices, roots):
+                    t_events[e].append(te)
+                    y_events[e].append(sol(te))
+
+                # NEW: Stop the integration after event_count_max events have occured
+                if event_count_end is not None:
+                    event_count += 1
+                    if event_count >= event_count_end:
+                        terminate = True
+
+                if terminate:
+                    status = 1
+                    t = roots[-1]
+                    y = sol(t)
+
+            g = g_new
+
+        if t_eval is None:
+            ts.append(t)
+            ys.append(y)
+        else:
+            t_eval_i_new = np.searchsorted(t_eval, t, side='right')
+            t_eval_step = t_eval[t_eval_i:t_eval_i_new]
+            if t_eval_step.size > 0:
+                if sol is None:
+                    sol = solver.dense_output()
+                ts.append(t_eval_step)
+                ys.append(sol(t_eval_step))
+                t_eval_i = t_eval_i_new
+
+    if event_count_end is not None and status == 0:
+        print("Warning: the time limit tf={:.1f} was reached before {:.0f} crossings could occur".format(tf,event_count_end))
+    
+    if t_events is not None:
+        t_events = [np.asarray(te) for te in t_events]
+        y_events = [np.asarray(ye) for ye in y_events]
+
+    if t_eval is None:
+        ts = np.array(ts)
+        ys = np.vstack(ys).T
+    elif ts:
+        ts = np.hstack(ts)
+        ys = np.hstack(ys)
+
+
+    return OptimizeResult(t=ts,y=ys,t_events = t_events, y_events = y_events,status=status)
