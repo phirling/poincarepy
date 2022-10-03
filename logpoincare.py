d5e5aff2850a28fa3215b10e4f9f5d24653c1c18
Patrick Hirling <patrick.hirling@epfl.ch>
2022-10-03 20:23:10 +0200

    Tomographic step 3

--- logpoincare.py
+++ logpoincare.py
@@ -44,6 +44,8 @@ parser.add_argument("-nb_orbs",type=int,default=11,help="Number of orbits to sam
 parser.add_argument("-Emin",type=float,default=20)
 parser.add_argument("-Emax",type=float,default=200)
 parser.add_argument("-nb_E",type=int,default=5,help="Number of energy slices in tomographic mode")
+parser.add_argument("--save",action='store_true')
+parser.add_argument("-open",type=str,default=None)
 
 args = parser.parse_args()
 
@@ -295,7 +297,7 @@ if args.mode == "interactive" or args.mode == "orbitfinder":
     fig.canvas.mpl_connect('button_press_event', onpick)
 
 # Mode 2: Draw a set of orbits in advance, show xy-orbit by clicking
-elif args.mode == "fill":
+elif args.mode == "fill": # Actually just a special case of tomographic
     class OrbitPicker:
         def __init__(self,ax_poincare,ax_orbit,E):
             self.line_orbits, = ax_orbit.plot([], [],lw=1)
@@ -356,42 +358,71 @@ elif args.mode == "fill":
     Picker.integrate()
 
 elif args.mode == "tomographic":
-    # Create empty line objects in the figure to speed up switching between E frames
-    lines_sections = [ax[0].plot([], [],'o',ms=0.3,color='black')[0] for i in range(args.nb_orbs)]
-    lines_orbits = [ax[1].plot([], [],lw=1)[0] for i in range(args.nb_orbs)]
     class OrbitEnsemble:
-        def __init__(self,potential,line1,line2,E):
+        def __init__(self,potential,lines_poincare,line_orbit,E,integrate=True):
             self.E = E
-            self.line1 = line1
-            self.line2 = line2
+            self.N_orbits = len(lines_poincare)
+            self.line1 = lines_poincare
+            self.line2 = line_orbit
             self.orbits = []
             self.sections = []
             self.pot = potential
-            self.integrate()
+            if integrate: self.integrate()
+            self.firstpick = True
         def integrate(self):
             xlim = 0.9999*self.pot.maxval_x(self.E)
-            x_ic = np.linspace(-xlim,xlim,args.nb_orbs)
-            ydot_ic = np.sqrt(2*(self.E-pot.potential([x_ic,np.zeros(args.nb_orbs)])))
-            for k in range(args.nb_orbs):
+            x_ic = np.linspace(-xlim,xlim,self.N_orbits)
+            ydot_ic = np.sqrt(2*(self.E-self.pot.potential([x_ic,np.zeros(self.N_orbits)])))
+            for k in range(self.N_orbits):
                 y0 = [x_ic[k],0,0,ydot_ic[k]]
                 res = solver.integrate_orbit(RHS,t_span,y0,t_eval=t_eval,events=event_yplanecross,event_count_end=event_count_max)
 
                 self.orbits.append(res['y'])
                 yevs = res['y_events'][0].T
                 self.sections.append(yevs[[0,2]])
-        def update_plot(self):
-            for i in range(args.nb_orbs):
-                self.line1[i].set_xdata(self.sections[i][0])
-                self.line1[i].set_ydata(self.sections[i][1])
-            self.line1[0].axes.relim()
-            self.line1[0].axes.autoscale()
-            self.line1[0].figure.canvas.draw()
+        def show(self):
+            if self.sections: # Don't show anything if empty
+                for i in range(self.N_orbits):
+                    self.line1[i].set_xdata(self.sections[i][0])
+                    self.line1[i].set_ydata(self.sections[i][1])
+                self.line1[0].axes.relim()
+                self.line1[0].axes.autoscale()
+                self.line1[0].figure.canvas.draw()
+                self.cid = self.line1[0].figure.canvas.mpl_connect('pick_event',self)
+        def hide(self):
+            if not self.firstpick:
+                self.prev_artist.set_color('black')
+                self.prev_artist.set_markersize(0.3)
+            self.line1[0].figure.canvas.mpl_disconnect(self.cid)
+        def __call__(self,event):
+            if self.firstpick:
+                self.firstpick = False
+            else:
+                self.prev_artist.set_color('black')
+                self.prev_artist.set_markersize(0.3)
+            event.artist.set_color('red')
+            event.artist.set_markersize(1.5)
+            
+            artistidx = self.line1.index(event.artist)
+            self.line2.set_xdata(self.orbits[artistidx][0])
+            self.line2.set_ydata(self.orbits[artistidx][1])
+            self.line2.axes.axis('equal')
+            self.line2.axes.relim()
+            self.line2.axes.autoscale()
+            event.artist.figure.canvas.draw()
+            self.prev_artist = event.artist
+        def set_data(self,sections,orbits):
+            # check if not empty
+            self.orbits = orbits
+            self.sections = sections
+        def get_data(self):
+            return self.sections, self.orbits
 
     class TomographicPlot:
         def __init__(self,fig,ensemblelist):
-            self.ensemblelist = ensemblelist
             self.idx = 0
-            self.ensemblelist[self.idx].update_plot()
+            self.ensemblelist = ensemblelist
+            self.ensemblelist[self.idx].show()
             fig.canvas.mpl_connect('key_press_event',self)
         def __call__(self,event):
             if event.key == 'up':
@@ -399,18 +430,46 @@ elif args.mode == "tomographic":
             elif event.key == 'down':
                 ii = self.idx - 1
             if ii in range(len(self.ensemblelist)):
-                self.ensemblelist[ii].update_plot()
+                self.ensemblelist[self.idx].hide()
+                self.ensemblelist[ii].show()
                 self.idx = ii
+        def get_data(self):
+            data = []
+            for oe in self.ensemblelist:
+                data.append(oe.get_data())
+            return data
+        def set_data(self,data):
+            for i,oe in enumerate(ensemblelist):
+                oe.set_data(data[i][0],data[i][1])
 
+    # Format to output/input data:
+    # list with dim 0 == ensemble index
+    # dim 1: 0 == sections, 1 == orbits
+    # dim 2 == number of orbit in ensemble
     ##### TESTS
-    erange = np.linspace(args.Emin,args.Emax,args.nb_E)
-    enss = []
+    import pickle as pkl
+    # Note: pickling does not work because of figure/axes links. Do not use this function yet
+    # Create empty line objects in the figure to speed up switching between E frames
+    lines_sections = [ax[0].plot([], [],'o',ms=0.3,color='black',picker=True)[0] for i in range(args.nb_orbs)]
+    line_orbit = ax[1].plot([], [],lw=1)[0]
+    E_range = np.linspace(args.Emin,args.Emax,args.nb_E)
+    ensemblelist = []
+    is_new = args.open is None
     print("Generating plots for {:.0f} energy levels...".format(args.nb_E))
-    for e in tqdm(erange):
-        enss.append(OrbitEnsemble(pot,lines_sections,lines_orbits,e))
+    for e in tqdm(E_range):
+        ensemblelist.append(OrbitEnsemble(pot,lines_sections,line_orbit,e,is_new))
 
-    tom = TomographicPlot(fig,enss)
+    tom = TomographicPlot(fig,ensemblelist)
+    if not is_new:
+        with open(args.open,'rb') as f:
+            imported_data = pkl.load(f)
+        tom.set_data(imported_data)
+        tom.ensemblelist[0].show()
     
+    if args.save:
+        with open("ensemblelist.pkl",'wb') as f:
+            pkl.dump(tom.get_data(),f)
+
 elif args.mode == "test":
     pass
 else:
