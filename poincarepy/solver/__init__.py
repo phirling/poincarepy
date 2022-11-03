3c41e9299dfafddb35f3987586a8fabddca02e57
Patrick Hirling <patrick.hirling@epfl.ch>
2022-11-03 11:40:21 +0100

    BSD license in scipy source (integrator)

--- poincarepy/solver/__init__.py
+++ poincarepy/solver/__init__.py
@@ -3,11 +3,39 @@ from scipy.integrate._ivp.rk import DOP853
 from scipy.integrate._ivp.ivp import prepare_events, handle_events, find_active_events, OdeResult
 
 '''
-Custom wrapper around scipy's DOP853 integrator, largely inspired by the solve_ivp method
-but containing some additional features and discarding functionalities not required by this program.
+Copyright (c) 2001-2002 Enthought, Inc. 2003-2022, SciPy Developers.
+All rights reserved.
 
-DISCLAIMER: Some portions of the code are copied as is from scipy's source,
-            specifically scipy/integrate/ivp.py).
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions
+are met:
+
+1. Redistributions of source code must retain the above copyright
+   notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above
+   copyright notice, this list of conditions and the following
+   disclaimer in the documentation and/or other materials provided
+   with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+   contributors may be used to endorse or promote products derived
+   from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+Modified version of SciPy's solve_ivp (scipy/integrate/ivp.py), to be able to terminate integration after a given
+number of events occured.
 
 Newly added:
 The user can either specify a time span (ti,tf) for the integration OR specify a number of times a
@@ -20,11 +48,6 @@ the case, expect false behaviour. Nonetheless, since the event watching code is 
 original solve_ivp, it still handles events as if multiple could be provided.
 TODO: either rewrite to make it explicit that only 1 event is watched OR implement a counting
       that also works for multiple events.
-
-Parameters
----------
-fun: callable
-...
 '''
 
 # The following function is a remade version of solve_ivp, which reuses some parts of the original
